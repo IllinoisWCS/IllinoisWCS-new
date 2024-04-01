7b5215b3e9fa6c1cdfefad013efd10edb87b850d
neha-vard <neha.vard@gmail.com>
2024-04-01 18:43:43 -0500

    Config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
-  basePath: "/IllinoisWCS-new/",
+  basePath: "/IllinoisWCS-new",
 };
 
 module.exports = nextConfig;
